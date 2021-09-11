a35373ad8ed86a696ef27e7106c8ec33723e575b
Paweł Ciosek <ciosek.pawel@gmail.com>
2021-09-11 17:53:18 +0200

    optimized php image size

--- api/docker/php/docker-entrypoint.sh
+++ api/docker/php/docker-entrypoint.sh
@@ -6,7 +6,6 @@ if [ "${1#-}" != "$1" ]; then
 	set -- php-fpm "$@"
 fi
 
-APP_ENV=${JWT_PASSPHRASE:-$(grep ''^APP_ENV='' .env | cut -f 2 -d ''='')}
 if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 	PHP_INI_RECOMMENDED="$PHP_INI_DIR/php.ini-production"
 	if [ "$APP_ENV" != 'prod' ]; then
